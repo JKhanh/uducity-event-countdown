fdde802e31cd1d936b7d420b728d7a781569488e
Khanh Nguyen <nguyenkhanhcv@gmail.com>
2024-09-04 16:01:47 +0700

    Initial Commit

---
+++ Event Countdown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Event Countdown
+//
+//  Created by Khanh on 4/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Event Countdown/Event_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_CountdownApp.swift
+//  Event Countdown
+//
+//  Created by Khanh on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Event_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
